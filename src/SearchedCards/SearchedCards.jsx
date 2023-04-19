65ab7a286b05a7e84311021f562b5443ed4217bd
Connor Clark <connordeclark@gmail.com>
2023-04-19 11:10:02 -0500

    Text his behind images

--- src/SearchedCards/SearchedCards.jsx
+++ src/SearchedCards/SearchedCards.jsx
@@ -7,6 +7,7 @@ export const SearchedCards = ({card}) => {
     <div className="card">
         <div className="flexBoxLeft">
             <img src={card.image_uris.art_crop} alt={card.name} />
+            {/* This will also have buttons */}
         </div>
         <div className="flexBoxRight">
             <h2>{card.name}</h2>
